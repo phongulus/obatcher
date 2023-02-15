b9fe8b118c4f5c019fb54ee317f9bfb6f5d5f3d2
koonwen <koonwen@gmail.com>
2023-02-16 03:17:18 +0800

    Update Linux info dump

--- benchmarks/bench.sh
+++ benchmarks/bench.sh
@@ -6,7 +6,7 @@ printf "\n\n%s\n" "$NOW" >> $LOG
 if [[ $OSTYPE == 'darwin'* ]]; then
   sysctl -a | grep machdep.cpu >> ./test_stats.txt
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    cat /proc/info >> ./test_stats.txt
+    lscpu >> ./test_stats.txt
 fi
 
 make -s >> ./test_stats.txt
